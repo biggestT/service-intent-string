ef8fff88f6494a629b821d2e95a61985d73a5182
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2017-03-13 17:04:00 +0100

    add parseFragment to build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { parseComponent, parseIntent  } from './parse.js';
+import { parseComponent, parseIntent, parseFragment  } from './parse.js';
 import { serializeComponent, serializeIntent } from './serialize.js';
 
 module.exports = {
   serializeComponent,
   serializeIntent,
   parseComponent,
-  parseIntent
+  parseIntent,
+  parseFragment
 };
