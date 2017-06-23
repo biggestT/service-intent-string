f535db70858d7e46989f2ee7c14a52a2b2f6d332
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2017-06-23 02:13:58 +0200

    [webpack] all in one conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,60 @@
 const path = require('path');
+const webpack = require('webpack');
+
+const context = path.resolve(__dirname + '/src');
+const entry = './index.js';
+const moduleConf = {
+  rules: [{
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: [ 'babel-loader' ]
+  }]
+};
+
+const configCommonJs = {
+  context,
+  entry,
+  module: moduleConf,
+  output: {
+    filename: 'sis.node.js',
+    libraryTarget: 'commonjs'
+  }
+};
+
+const configLegacy = {
+  context,
+  entry,
+  module: moduleConf,
+  output: {
+    filename: 'sis.min.js',
+    library: 'ServiceIntentString',
+    libraryTarget: 'var'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.js$/,
+      compressor: { warnings: false },
+      sourceMap: true,
+      compress: { drop_console: true },
+      minimize: true
+    })
+  ]
+};
 
 const config = {
-  context: path.resolve(__dirname + '/src'),
-  entry: './index.js',
-
+  context,
+  entry,
+  module: moduleConf,
   output: {
     filename: 'sis.js',
     library: 'service-intent-string',
     libraryTarget: 'amd'
-  },
-
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: [ 'babel-loader' ]
-    }]
   }
 };
 
-module.exports = config;
+module.exports = [
+  config,
+  configLegacy,
+  configCommonJs
+];
 
