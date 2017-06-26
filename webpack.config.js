4a2bb586dfadc9ffc4d289328ad8bb22f3264d31
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2017-06-26 21:37:43 +0200

    Commonjs (#1)
    * [webpack] all in one conf
    
    * [webpack] default to build commonjs lib
    
    * cleanup
    
    * [webpack] no redunant key

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,48 @@
 const path = require('path');
+const webpack = require('webpack');
+
+const context = path.resolve(__dirname + '/src');
+const entry = './index.js';
+const moduleConf = {
+  rules: [{
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: [ 'babel-loader' ]
+  }]
+};
+
+const configLegacy = {
+  context,
+  entry,
+  module: moduleConf,
+  output: {
+    filename: 'sis.min.js',
+    library: 'ServiceIntentString',
+    libraryTarget: 'var'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.js$/,
+      compressor: { warnings: false },
+      sourceMap: true,
+      compress: { drop_console: true },
+      minimize: true
+    })
+  ]
+};
 
 const config = {
-  context: path.resolve(__dirname + '/src'),
-  entry: './index.js',
-
+  context,
+  entry,
+  module: moduleConf,
   output: {
     filename: 'sis.js',
-    library: 'service-intent-string',
-    libraryTarget: 'amd'
-  },
-
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: [ 'babel-loader' ]
-    }]
+    libraryTarget: 'commonjs'
   }
 };
 
-module.exports = config;
+module.exports = [
+  config,
+  configLegacy
+];
 
